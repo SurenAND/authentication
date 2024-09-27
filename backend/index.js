155677bbbe2d475b8578f2da500d6582fd7a9e8b
AshkanZjj <140895184+SurenAND@users.noreply.github.com>
2024-09-27 15:21:04 +0330

    feat: create backend & connect it to mongoDB

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import { connectDB } from "./db/connectDB.js";
+import authRouter from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allow us to parse json bodies in requests
+
+app.use("/api/auth", authRouter);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
