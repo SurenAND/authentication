5bb343902ecec61e02eca6ec111653bd0d50c971
AshkanZjj <140895184+SurenAND@users.noreply.github.com>
2024-09-28 12:50:50 +0330

    Develop (#5)
    * feat: create backend & connect it to mongoDB
    
    * feat: create user model
    
    * feat: create authController & authRoute
    
    * feat: config mailtrap
    
    * feat: add email verification
    
    * feat: add user logout
    
    * feat: add forget & reset password
    
    * feat: check authentication
    
    * fix: change some endpoints name
    
    * feat: add postman collection json
    
    * feat: install cors & config it (#2)
    
    * Frontend (#3)
    
    * feat: init & install packages
    
    * feat: create FloatingShape for background
    
    * feat: signup page
    
    * feat: login page
    
    * feat: verify code page
    
    * feat: add toaster
    
    * feat: add check auth
    
    * feat: home/dashboard page
    
    * feat: forgot password page
    
    * feat: reset password page
    
    * feat: redirect to home if the route does not exist
    
    * Backend (#4)
    
    * feat: install cors & config it
    
    * feat: resolve path in production
    
    * feat: add .env file to git ignore

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 import authRouter from "./routes/auth.route.js";
@@ -9,11 +11,27 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json()); // allow us to parse json bodies in requests
 app.use(cookieParser()); // allow us to parse cookies in requests
 
 app.use("/api/auth", authRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
