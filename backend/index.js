a7b0587b1d0d3e58d3febbf5c52d2248f9724754
AshkanZjj <140895184+SurenAND@users.noreply.github.com>
2024-09-28 12:42:20 +0330

    feat: resolve path in production

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 import authRouter from "./routes/auth.route.js";
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -22,6 +24,14 @@ app.use(express.json()); // allow us to parse json bodies in requests
 app.use(cookieParser()); // allow us to parse cookies in requests
 
 app.use("/api/auth", authRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
